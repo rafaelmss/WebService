22930a593b9d2301210d775ecc4bfe176018b18b
B2ML-Projetos <projetos@b2ml.com.br>
2017-06-15 10:55:42 -0300

    first commit

---
+++ WebService/Controllers/EchoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    [RoutePrefix("api/echo")]
+    public class EchoController : ApiController
+    {
+        [AcceptVerbs("POST")]
+        [Route("EchoPost")]
+        public string EchoPost([FromBody] dynamic data)
+        {
+            return "Echo-"+ data.nome+" "+data.sobrenome;
+        }
+        
+        [AcceptVerbs("GET")]
+        [Route("EchoGet/{value}")]
+        public string EchoGet(string value)
+        {
+            return "Echo-" + value;
+        }
+    }
+}
